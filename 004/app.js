e95488c5f5ab1b841ff12b299613c0c4dae25f0e
Ken Schnall <kenneth.schnall@gmail.com>
2016-04-12 13:49:12 -0400

    finish 004

--- 004/app.js
+++ 004/app.js
@@ -1,1 +1,18 @@
 // MY FIRST ASYNC I/O!
+
+var fs = require('fs');
+
+function collect(err, data) {
+	fileStringArray = data.toString().split("\n");
+	newLines=0;
+
+	fileStringArray.forEach(function(currentValue) {
+		newLines+=1;
+	});
+
+	newLines-=1;
+
+	console.log(newLines);
+}
+
+var fileString = fs.readFile(process.argv[2], collect);
